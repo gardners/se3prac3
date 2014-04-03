290da98f331800b11c949010d864b22276903b3c
schr0082 <schr0082@uni.flinders.edu.au>
2014-04-03 13:57:06 +1030

    refined pass left function and added merging function. all provided tests pass.

--- tilt.c
+++ tilt.c
@@ -6,15 +6,23 @@ int tilt_line_left(int length,int *line)
   // make sure vector length is sensible
   if (length<1||length>255) return -1;
 
-  // slide tiles to the left
+  // combine tiles as required
   int i;
-  for(i=1; i<=3; i++) {
-    if (line[i]!=0 && line[i-1]==0) {
-      line[i-1]=line[i];
-      line[i]=0;
+  for(i=0; i<=2; i++) {
+    if (line[i]==line[i+1]) {
+      line[i]=line[i]*2;
+      line[i+1]=0;
+    }
+  }
+
+  // slide tiles to the left
+  int j;
+  for(j=3; j>=1; j--) {
+    if (line[j]!=0 && line[j-1]==0) {
+      line[j-1]=line[j];
+      line[j]=0;
     } 
   }
-  // combine tiles as required
 
   return 0;
 }
