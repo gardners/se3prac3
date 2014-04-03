1e9b387539f4726eae2fbeb0ef2cb4c5d5b2bff3
schr0082 <schr0082@uni.flinders.edu.au>
2014-04-03 15:39:59 +1030

    fixed the issue of 0,2,0,2 not becoming 4,0,0,0 by adding another shift loop

--- tilt.c
+++ tilt.c
@@ -6,6 +6,18 @@ int tilt_line_left(int length,int *line)
   // make sure vector length is sensible
   if (length<1||length>255) return -1;
 
+  // slide tiles to the left
+  int k;
+  for(k=2; k>0; k--) {
+    int j;
+    for(j=3; j>=1; j--) {
+      if (line[j]!=0 && line[j-1]==0) {
+        line[j-1]=line[j];
+        line[j]=0;
+      } 
+    }
+  }
+
   // combine tiles as required
   int i;
   for(i=0; i<=2; i++) {
@@ -14,14 +26,13 @@ int tilt_line_left(int length,int *line)
       line[i+1]=0;
     }
   }
-
-  // slide tiles to the left
-  int j;
-  for(j=3; j>=1; j--) {
-    if (line[j]!=0 && line[j-1]==0) {
-      line[j-1]=line[j];
-      line[j]=0;
-    } 
+  
+  int h;
+  for(h=3; h>=1; h--) {
+    if (line[h]!=0 && line[h-1]==0) {
+      line[h-1]=line[h];
+      line[h]=0;
+     }
   }
 
   return 0;
