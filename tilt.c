887608bf2fe8d95fa6ddbd3f0074ddab0d9a17e5
schr0082 <schr0082@uni.flinders.edu.au>
2014-04-03 13:37:14 +1030

    added shift left function. only shifts one space at the moment.

--- tilt.c
+++ tilt.c
@@ -7,7 +7,13 @@ int tilt_line_left(int length,int *line)
   if (length<1||length>255) return -1;
 
   // slide tiles to the left
-
+  int i;
+  for(i=1; i<=3; i++) {
+    if (line[i]!=0 && line[i-1]==0) {
+      line[i-1]=line[i];
+      line[i]=0;
+    } 
+  }
   // combine tiles as required
 
   return 0;
