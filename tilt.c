a9b9d114642ee8973d8797308c041d92645930d1
James Pogson <pogs0004@uni.flinders.edu.au>
2014-04-03 15:23:12 +1030

    Working on tilt left logic, so far to no avail. But the future looks bright.

--- tilt.c
+++ tilt.c
@@ -7,8 +7,32 @@ int tilt_line_left(int length,int *line)
   if (length<1||length>255) return -1;
 
   // slide tiles to the left
+  int i;
+    for(i=0;i<=2;i++){
+      if(line[i]==line[i+1]){
+        line[i] = line[i]*2;
+        line[i+1] = 0;
+    }
+  }
 
   // combine tiles as required
+int j;
+ for(j=0;j<2;j++){
+  for(i=3;i>=1;i++){
+   if(line[i] != 0 && line[i-1] == 0){
+    line[i-1] = line[i];
+    line[i] = 0;
+    }
+  }
+ }
+
+
+    for(i=0;i<=2;i++){
+      if(line[i]==line[i+1]){
+        line[i] = line[i]*2;
+        line[i+1] = 0;
+    }
+  }
 
   return 0;
 }
