bd0892e3d6208ee620889f90370a9a1c5220af25
James Pogson <pogs0004@uni.flinders.edu.au>
2014-04-10 15:21:09 +0930

    Fixed tilt.c to pass inital tests

--- tilt.c
+++ tilt.c
@@ -3,42 +3,40 @@
 
 int tilt_line_left(int length,int *line)
 {
+
   // make sure vector length is sensible
   if (length<1||length>255) return -1;
 
   // slide tiles to the left
 
-    if (line[0] + line[1] + line[2] + line[3] == 0){
-    }else{
-//this is where I left off 3/4/2014
+  int j;
 
-}
-//  int i;
-//    for(i=0;i<=2;i++){
-//      if(line[i]==line[i+1]){
-//        line[i] = line[i]*2;
-//        line[i+1] = 0;
-//    }
-//  }
-
-  // combine tiles as required
-int j;
- for(j=0;j<2;j++){
-  for(i=3;i>=1;i++){
-   if(line[i] != 0 && line[i-1] == 0){
-    line[i-1] = line[i];
-    line[i] = 0;
+  for (j=2; j>0; j--){
+    int i;
+    for(i=3;i>=1;i--){
+      if(line[i] != 0 && line[i-1] == 0){
+        line[i-1] = line[i];
+        line[i] = 0;
+      }
     }
   }
- }
 
-
-    for(i=0;i<=2;i++){
-      if(line[i]==line[i+1]){
-        line[i] = line[i]*2;
-        line[i+1] = 0;
+ //combine tiles
+int k;
+   for(k=0;k<=2;k++){
+      if(line[k]==line[k+1]){
+        line[k] = line[k]*2;
+        line[k+1] = 0;
     }
   }
+
+int h;
+for(h=3;h>=1;h--){
+      if(line[h] != 0 && line[h-1] == 0){
+        line[h-1] = line[h];
+        line[h] = 0;
+      }
+    }
 
   return 0;
 }
