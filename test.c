b48dd342f6781de22f80488ef1ba907302652fe5
James Pogson <pogs0004@uni.flinders.edu.au>
2014-04-10 15:24:48 +0930

    Added test for correct tilt and combine of 0,2,0,2

--- test.c
+++ test.c
@@ -43,6 +43,8 @@ int test_tilt_left()
   e|=ttl_vector(0,0,1,1,NULL,2,0,0,0);
   e|=ttl_vector(4,0,1,1,NULL,4,2,0,0);
   e|=ttl_vector(2,0,1,1,NULL,2,2,0,0);
+  e|=ttl_vector(0,2,0,2,"Values cascade and combine",4,0,0,0);
+
   return e;
 }
 
