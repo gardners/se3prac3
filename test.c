503b8c6e0445b249e743a1fa5aaf9f0509df3e0a
James Pogson <pogs0004@uni.flinders.edu.au>
2014-05-09 00:39:45 +0930

    Added tests for 4x4 board, based on expanding 2x2 and 3x3 tests to test.c

--- test.c
+++ test.c
@@ -129,10 +129,86 @@ int board3_vector_test(int i1,int i2,int i3,int i4,int i5,
   return 0;
 }
 
+// 4x4 test//////////////////////////////////////////////////
+
+int board4_vector_test(int i1, int i2, int i3, int i4,
+    int i5, int i6, int i7, int i8,
+    int i9, int i10, int i11, int i12,
+    int i13, int i14, int i15, int i16, char *msg,
+    int o1, int o2, int o3, int o4, int o5, int o6, int o7, int o8,
+    int o9, int o10, int o11, int o12, int o13, int o14, int o15, int o16,
+    int (*func)(int,int **)){
+
+    int **board=alloca(4*sizeof(int*));
+    board[0] = alloca(sizeof(int)*4);
+    board[1] = alloca(sizeof(int)*4);
+    board[2] = alloca(sizeof(int)*4);
+    board[3] = alloca(sizeof(int)*4);
+
+    board[0][0] = i1;
+    board[1][0] = i2;
+    board[2][0] = i3;
+    board[3][0] = i4;
+    board[0][1] = i5;
+    board[1][1] = i6;
+    board[2][1] = i7;
+    board[3][1] = i8;
+    board[0][2] = i9;
+    board[1][2] = i10;
+    board[2][2] = i11;
+    board[3][2] = i12;
+    board[0][3] = i13;
+    board[1][3] = i14;
+    board[2][3] = i15;
+    board[3][3] = i16;
+
+    if (msg){
+      printf("%s - ", msg);
+    } else {
+      printf("Board operation on {{%d, %d, %d, %d},...} yeilds {{%d, %d, %d, %d}...} - ", i1, i2, i3, i4, o1, o2, o3, o4);
+    }
+
+    fflush(stdout);
+    func(4, board);
+
+    if ((board[0][0] != o1) ||
+        (board[1][0] != o2) ||
+        (board[2][0] != o3) ||
+        (board[3][0] != o4) ||
+        (board[0][1] != o5) ||
+        (board[1][1] != o6) ||
+        (board[2][1] != o7) ||
+        (board[3][1] != o8) ||
+        (board[0][2] != o9) ||
+        (board[1][2] != o10) ||
+        (board[2][2] != o11) ||
+        (board[3][2] != o12) ||
+        (board[0][3] != o13) ||
+        (board[1][3] != o14) ||
+        (board[2][3] != o15) ||
+        (board[3][3] != o16)){
+
+    printf("FAILED:\n");
+
+    return -1;
+
+    }
+
+  printf("PASSED.\n");
+  
+  return 0;
+
+}
+
+
 int main(int argc,char **argv)
 {
   int e=0;
   e|=test_tilt_left();
+
+
+
+// NOTE: should put all below tests in seperate function as done with above e|=test_tilt_left();
 
 // 2x2 rotate 90 tests /////////////////////////////////////
   board_vector_test(1,2,3,4,"Rotate 2x2 board right (+90)",3,1,4,2,board_rotate_90);
@@ -145,13 +221,18 @@ int main(int argc,char **argv)
 
 // 3x3 rotate 90 tests /////////////////////////////////////
   board3_vector_test(1,2,3,4,5,6,7,8,9,"Rotate 3x3 +90 R",7,4,1,8,5,2,9,6,3,board_rotate_90);
-  board3_vector_test(1,2,3,4,5,6,7,8,9,"Rotate 3x3 +90 R",7,4,1,8,5,2,9,6,3,board_rotate_90);
-  board3_vector_test(1,2,3,4,5,6,7,8,9,"Rotate 3x3 +90 R",7,4,1,8,5,2,9,6,3,board_rotate_90);
+  board3_vector_test(9,8,7,6,5,4,3,2,1,"Rotate 3x3 +90 R",3,6,9,2,5,8,1,4,7,board_rotate_90);
+  board3_vector_test(0,0,0,1,1,1,2,2,2,"Rotate 3x3 +90 R",2,1,0,2,1,0,2,1,0,board_rotate_90);
 
 // 3x3 rotate 180 tests /////////////////////////////////////
   board3_vector_test(1,2,3,4,5,6,7,8,9,"Rotate 3x3 +180 R",7,4,1,8,5,2,9,6,3,board_rotate_180);
-  board3_vector_test(1,2,3,4,5,6,7,8,9,"Rotate 3x3 +180 R",7,4,1,8,5,2,9,6,3,board_rotate_180);
-  board3_vector_test(1,2,3,4,5,6,7,8,9,"Rotate 3x3 +180 R",7,4,1,8,5,2,9,6,3,board_rotate_180);
+  board3_vector_test(9,8,7,6,5,4,3,2,1,"Rotate 3x3 +180 R",3,6,9,2,5,8,1,4,7,board_rotate_180);
+  board3_vector_test(0,1,0,1,0,1,0,1,0,"Rotate 3x3 +180 R",0,1,0,1,0,1,0,1,0,board_rotate_180);
+
+
+// 4x4 rotate 90 tests /////////////////////////////////////
+  board4_vector_test(1,2,3,4, 5,6,7,8, 1,2,3,4, 5,6,7,8, "Rotate 4x4 board right (+90)"
+                    ,4,3,2,1, 8,7,6,5, 4,3,2,1, 8,7,6,5, board_rotate_90);
 
 
 
