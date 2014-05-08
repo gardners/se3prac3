a9cbbac02bce9139ffb2380f9bcc39a42b1e447a
schr0082 <schr0082@uni.flinders.edu.au>
2014-05-08 15:21:18 +0930

    added rotate 90 method. passes first rotate 90 test. now to add more tests

--- board.c
+++ board.c
@@ -35,6 +35,28 @@ int board_display(int size,int **board)
 }
 
 int board_rotate_90(int size, int **board) {
-  return 0;
+
+  //check to see if the board is an appropriate size
+
+  if (size < 1 || size > 255) return -1;
+  
+  int temp_board[size][size];
+  int x,y;
+
+
+  // rotate into temp_board
+
+  for(x = 0; x < size; x++)
+    for(y = 0; y < size; y++)
+      temp_board[(size-1)-y][x]=board[x][y];
+
+
+  // copy temp board back into original board
+
+  for(x = 0; x < size; x++) 
+    for(y = 0; y < size; y++)
+      board[x][y]=temp_board[x][y];
+ 
+ return 0;
 }
 
