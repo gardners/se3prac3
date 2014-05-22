59a0bb110b11d4135ee60863d27adc8a35f04056
schr0082 <schr0082@uni.flinders.edu.au>
2014-05-22 14:32:55 +0930

    added a 3x3 board in test.c  Will now proceed to add tests for 3x3 board

--- test.c
+++ test.c
@@ -77,11 +77,63 @@ int board_vector_test(int i1,int i2,int i3,int i4,char *msg,
   return 0;
 }
 
+int board3_vector_test(int i1,int i2,int i3,int i4,int i5,
+
+		       int i6,int i7,int i8,int i9,char *msg,
+
+		       int o1,int o2,int o3,int o4,int o5,
+
+		       int o6,int o7,int o8,int o9,
+
+		       int (*func)(int,int **))
+
+{
+
+  int **board=alloca(3*sizeof(int*));
+  board[0]=alloca(sizeof(int)*3);
+  board[1]=alloca(sizeof(int)*3);
+  board[2]=alloca(sizeof(int)*3);
+  board[0][0]=i1; board[1][0]=i2; board[2][0]=i3;
+  board[0][1]=i4; board[1][1]=i5; board[2][1]=i6;
+  board[0][2]=i7; board[1][2]=i8; board[2][2]=i9;
+
+  if (msg) printf("%s - ",msg); 
+
+  else {
+    printf("Board operation on {{%d,%d,%d},...} yields {{%d,%d,%d}...} - ",
+	     i1,i2,i3,o1,o2,o3);
+  }
+
+  fflush(stdout);
+  func(3,board);
+
+  if (
+      (board[0][0]!=o1)||(board[1][0]!=o2)||(board[2][0]!=o3)||
+      (board[0][1]!=o4)||(board[1][1]!=o5)||(board[2][1]!=o6)||
+      (board[0][2]!=o7)||(board[1][2]!=o8)||(board[2][2]!=o9)
+     ) {
+      printf("FAILED: {{%d,%d,%d},{%d,%d,%d},{%d,%d,%d}} became {{%d,%d,%d},{%d,%d,%d},{%d,%d,%d}} instead of"
+	     " {{%d,%d,%d},{%d,%d,%d},{%d,%d,%d}}\n",
+	     i1,i2,i3,i4,i5,i6,i7,i8,i9,
+	     board[0][0],board[1][0],board[2][0],
+	     board[0][1],board[1][1],board[2][1],
+	     board[0][2],board[1][2],board[2][2],
+	     o1,o2,o3,o4,o5,o6,o7,o8,o9);
+             return -1;
+       } 
+
+  printf("PASSED.\n");
+  return 0;
+}
+
 int main(int argc,char **argv)
 {
   int e=0;
+
+  //tilt left test for one row
   e|=test_tilt_left();
 
+  //rotate tests for 2x2 board
   board_vector_test(1,2,4,8,"Rotate 2x2 board right (+90)",4,1,8,2,board_rotate_90);
   board_vector_test(0,1,2,4,"Rotate 2x2 board right (+90)",2,0,4,1,board_rotate_90);
   board_vector_test(0,0,0,1,"Rotate 2x2 board right (+90)",0,0,1,0,board_rotate_90);
@@ -96,6 +148,11 @@ int main(int argc,char **argv)
   board_vector_test(0,1,2,4,"Rotate 2x2 board right (+270)",1,4,0,2,board_rotate_270);
   board_vector_test(0,0,0,1,"Rotate 2x2 board right (+270)",0,1,0,0,board_rotate_270);
   board_vector_test(1,0,0,1,"Rotate 2x2 board right (+270)",0,1,1,0,board_rotate_270);
+
+  //rotate tests for 3x3 board
+
+
+  //rotate tests for 4x4 board
 
   return e;
 }
